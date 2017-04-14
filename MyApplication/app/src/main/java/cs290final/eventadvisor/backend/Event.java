e4e52b0822353fbab4792488a0ffb316f3363359
Jerry Pan <qp7@duke.edu>
2017-04-14 02:22:24 -0400

    added login

---
+++ MyApplication/app/src/main/java/cs290final/eventadvisor/backend/Event.java
@@ -0,0 +1,30 @@
+package cs290final.eventadvisor.backend;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Jerry on 4/13/2017.
+ */
+@IgnoreExtraProperties
+public class Event {
+    public String title;
+    public String date;
+    public String startTime;
+    public String endTime;
+    public String description;
+    public double longitude;
+    public double latitude;
+
+    public Event(){
+
+    }
+    public Event(String title, String date, String startTime, String endTime, String description, double longitude, double latitude){
+        this.title=title;
+        this.date=date;
+        this.startTime=startTime;
+        this.endTime=endTime;
+        this.description=description;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+}
---
+++ MyApplication/app/src/main/java/cs290final/eventadvisor/backend/SendEvent.java
@@ -0,0 +1,37 @@
+package cs290final.eventadvisor.backend;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Jerry on 4/13/2017.
+ */
+
+public class SendEvent {
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    public void SendEvent(){
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+}
