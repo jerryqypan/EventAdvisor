1ed64628775118b04b5dbfbb3835a4584458644b
limeymango <emilymeng1@gmail.com>
2017-04-18 02:51:01 -0400

    updated front page

---
+++ MyApplication/app/src/main/java/cs290final/eventadvisor/CreateEventActivity.java
@@ -0,0 +1,17 @@
+package cs290final.eventadvisor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by emilymeng on 4/16/17.
+ */
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
