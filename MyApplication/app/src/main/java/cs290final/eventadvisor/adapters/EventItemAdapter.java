cbb7d3ef20219b3024a326ed91bbc190d584094d
Chirag Tamboli <ct165@duke.edu>
2017-04-30 19:58:54 -0400

    added favorites support

--- MyApplication/app/src/main/java/cs290final/eventadvisor/adapters/EventItemAdapter.java
+++ MyApplication/app/src/main/java/cs290final/eventadvisor/adapters/EventItemAdapter.java
@@ -2,6 +2,8 @@ package cs290final.eventadvisor.adapters;
 
 import android.content.Context;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,13 +13,17 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import cs290final.eventadvisor.MapsActivity;
 import cs290final.eventadvisor.R;
 import cs290final.eventadvisor.backend.Event;
+import cs290final.eventadvisor.backend.SelectInterest;
 
 /**
  * Created by Asim Hasan on 4/30/2017.
@@ -62,6 +68,8 @@ public class EventItemAdapter extends RecyclerView.Adapter<EventItemAdapter.View
         public TextView eventTime;
         public TextView eventDescription;
         public ImageView eventImage;
+        public ImageButton favoriteImageButton;
+        public boolean isFavoriteClicked;
 
         public Event mEvent;
 
@@ -81,24 +89,33 @@ public class EventItemAdapter extends RecyclerView.Adapter<EventItemAdapter.View
                 }
             });
 
-            ImageButton favoriteImageButton =
-                    (ImageButton) itemView.findViewById(R.id.favorite_button);
-            favoriteImageButton.setOnClickListener(new View.OnClickListener(){
+            favoriteImageButton = (ImageButton) itemView.findViewById(R.id.favorite_button);
+            favoriteImageButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Snackbar.make(v, "Added to Favorite",
-                            Snackbar.LENGTH_LONG).show();
+                    MapsActivity mapsActivity = (MapsActivity) mContext;
+                    if (isFavoriteClicked) {
+                        isFavoriteClicked = false;
+                        favoriteImageButton.setImageResource(R.drawable.ic_favorite_off);
+                        new SelectInterest(favoriteImageButton).execute(mapsActivity.currentUser.getUid(), Integer.toString(mEvent.getIdEvent()), "delete");
+                        mEvent.setisInterested(isFavoriteClicked);
+                    } else {
+                        isFavoriteClicked = true;
+                        favoriteImageButton.setImageResource(R.drawable.ic_favorite_on);
+                        new SelectInterest(favoriteImageButton).execute(mapsActivity.currentUser.getUid(), Integer.toString(mEvent.getIdEvent()), "add");
+                        mEvent.setisInterested(isFavoriteClicked);
+                    }
                 }
             });
 
-            ImageButton shareImageButton = (ImageButton) itemView.findViewById(R.id.share_button);
-            shareImageButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Snackbar.make(v, "Share article",
-                            Snackbar.LENGTH_LONG).show();
-                }
-            });
+//            ImageButton shareImageButton = (ImageButton) itemView.findViewById(R.id.share_button);
+//            shareImageButton.setOnClickListener(new View.OnClickListener(){
+//                @Override
+//                public void onClick(View v) {
+//                    Snackbar.make(v, "Share article",
+//                            Snackbar.LENGTH_LONG).show();
+//                }
+//            });
         }
 
         public void setData(Event event) {
@@ -113,6 +130,10 @@ public class EventItemAdapter extends RecyclerView.Adapter<EventItemAdapter.View
             eventDate.setText(event.getDate());
             eventLocation.setText(event.getPlace());
             eventTime.setText(event.getStartTime());
+            isFavoriteClicked = event.getisInterested();
+            if (event.getisInterested()) {
+                favoriteImageButton.setImageResource(R.drawable.ic_favorite_on);
+            }
         }
 
         public void onEventClick(View v) {
