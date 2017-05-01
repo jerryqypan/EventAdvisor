c4eb8109fc0347bfb892d9f307306dc435b880f6
Chirag Tamboli <ct165@duke.edu>
2017-04-30 21:16:14 -0400

    added jdoc comments for jsonquizgen and most of event class

--- MyApplication/app/src/main/java/cs290final/eventadvisor/backend/Event.java
+++ MyApplication/app/src/main/java/cs290final/eventadvisor/backend/Event.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 /**
  * Created by Jerry on 4/13/2017.
+ * A class to hold the details of a specific event.
  */
 @IgnoreExtraProperties
 public class Event implements Parcelable {
@@ -45,67 +46,130 @@ public class Event implements Parcelable {
     }
 
 
-
+    /**
+     *
+     * @return The User ID of who created the event
+     */
     public String getUid() {
         return uid;
     }
 
-
+    /**
+     * Sets the user id of who created this event
+     * @param uid The user id
+     */
     public void setUid(String uid) {
         this.uid = uid;
     }
+
+    /**
+     *
+     * @return The title of this event
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the event.
+     * @param title     The title of the event
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     *
+     * @return  The start date of the event.
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Sets the date of this event.
+     * @param date  The date to set.
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     *
+     * @return The start time of this event.
+     */
     public String getStartTime() {
         return startTime;
     }
 
+    /**
+     * Sets the start time of this event
+     * @param startTime The start time.
+     */
     public void setStartTime(String startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     *
+     * @return The end time of this event.
+     */
     public String getEndTime() {
         return endTime;
     }
 
+    /**
+     * Sets the end time of this event
+     * @param endTime   The end time
+     */
     public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
 
+    /**
+     *
+     * @return  The description of this event.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of this event.
+     * @param description   The description of this event
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     *
+     * @return  The latitude of the location of this event.
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * Sets the latitude of the location of this event.
+     * @param latitude  The latitude of the location.
+     */
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+    /**
+     *
+     * @return  The longitude of the location of this event.
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * Sets the longitude of the location of this event.
+     * @param longitude The longitude of the location of this event.
+     */
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
@@ -114,16 +178,40 @@ public class Event implements Parcelable {
 
     public void setPlace(String place){this.place=place;}
 
+    /**
+     *
+     * @return  Returns true is the user is interested in this event.
+     */
     public boolean getisInterested(){return isInterested;}
+
     public void setisInterested(boolean isInterested){this.isInterested=isInterested;}
+
+    /**
+     *
+     * @return The event id.
+     */
     public int getIdEvent() {
         return idEvent;
     }
 
+    /**
+     * Sets the event id
+     * @param idEvent   The event id
+     */
     public void setIdEvent(int idEvent) {
         this.idEvent = idEvent;
     }
+
+    /**
+     *
+     * @return  Gets the URL of an image of this event.
+     */
     public String getUrl(){return url;}
+
+    /**
+     * Sets the image location url for this event
+     * @param url   The url of the image.
+     */
     public void setUrl(String url){this.url=url;}
 
     @Override
@@ -177,6 +265,10 @@ public class Event implements Parcelable {
             return new Event[size];
         }
     };
+
+    /**
+     * A wrapper class, designed to parse a list of JSON events.
+     */
     public static class EventsWrapper {
         private List<Event> events;
 
